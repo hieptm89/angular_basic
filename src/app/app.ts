fd0d7196b382439be7063ba1687921ef3eb6e38d
hieptm <hieptm@vnpt.vn>
2026-07-25 20:29:57 +0700

    second

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderLayoutComponent } from './shared/header-layout.component';
+import { FormsModule } from '@angular/forms';
+import { CurrencyPipe } from './pipes/CurrencyPipe.pipe';
+import { UpperCasePipe } from './pipes/UpperCasePipe.pipe';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderLayoutComponent],
+  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule, CurrencyPipe, UpperCasePipe, NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   // Text
-  title = 'Hello world';
+  title = '';
   user = {
     name: 'John Doe',
     old: 30
@@ -27,10 +31,37 @@ export class App {
   toggleDisabled() {
     this.isDisabled = !this.isDisabled;
   }
+  
+  sayHello(): void {
+    this.title = "Hello world, my name is " + this.user.name + ". I am " + this.user.old + " years old.";
+  }
 
   onBuyParent() {
     this.cartCountParent++;
     console.log('Mua hàng được click');
   }
+
+  // Two way binding
+  userName = "";
+
+  // List render
+  products = [
+    {
+      name: 'samba og',
+      price: 40000
+    },
+    {
+      name: 'n F1',
+      price: 50000
+    },
+    {
+      name: 'giày Adidas',
+      price: 60000
+    },
+    {
+      name: 'giày mlb',
+      price: 70000
+    }
+  ];
 
 }
